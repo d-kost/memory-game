5d2d5739f20a35b1edcb2f65ec9f96e3ec53f2ab
Daria Kostuchenko <dariakostuchenko90@gmail.com>
2021-11-07 17:34:41 +0800

    cards with images

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 import { ICard } from '../../common/Board'
 import { useAppDispatch } from '../../store/store'
@@ -12,18 +12,37 @@ interface ICardProps extends ICard {
 const Card: React.FC<ICardProps> = (props) => {
   const { index, id, removed, flipped } = props
   const dispatch = useAppDispatch()
+  const [logo, setLogo] = useState('')
 
   const flipCard = () => {
     dispatch(flip(index))
     dispatch(match(index))
   }
 
+  useEffect(() => {
+    import(`../../../assets/icons/${id}.svg`).then((obj) => {
+      setLogo(obj.default)
+    })
+  }, [])
+
   if (removed) {
     return <div className="card card-removed"></div>
   }
 
   if (flipped) {
-    return <div className="card">{id}</div>
+    return (
+      <div className="card">
+        <div className="card-content">
+          <img
+            className="card-icon"
+            src={logo}
+            alt="card-icon"
+            width="50px"
+            height="50px"
+          />
+        </div>
+      </div>
+    )
   }
 
   return <div className="card card-back" onClick={flipCard}></div>
